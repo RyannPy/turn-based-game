1ca1cbcb8eb38cde346ae048dcf2157c1a3d7b7f
Ryan <ryanbayu1233@gmail.com>
2026-05-04 17:55:59 +0700

    feat: skill 1 and skill 2 - v0.1

--- main.py
+++ main.py
@@ -23,15 +23,32 @@ class Hero(Character):
     super().__init__(name, hp, attack)
     self.sp = 3
     
-  def use_skill(self, target):
-    if self.sp > 0:
-      damage = self.attack * 2
+  def basic_attack(self, target):
+    target.hp -= self.attack
+    self.sp += 1
+    print(f"{self.name} menggunakan Basic Attack | Damage: {self.attack}")
+  
+  def skill_1(self, target):
+    if self.sp >= 1:
+      damage = self.attack * 1.5
       target.hp -= damage
       self.sp -= 1
-      
-      print(f"{self.name} menggunakan skill, damage: {damage}")
+      print(f"{self.name} menggunakan Skill 1, damage: {damage}")
     else:
       print("Skill Point tidak cukup")
+      return False
+    return True
+    
+  def skill_2(self, target):
+    if self.sp >= 2:
+      damage = self.attack * 2
+      target.hp -= damage
+      self.sp -= 2
+      print(f"{self.name} menggunakan Skill 2, damage: {damage}")
+    else:
+      print("Skill Point tidak cukup")
+      return False
+    return True
 
 class Enemy(Character):
   pass
@@ -44,28 +61,44 @@ def battle(hero, enemy):
   while hero.is_alive() and enemy.is_alive():
     print(f"{hero.name} HP: {hero.hp} | SP: {hero.sp} | {enemy.name} HP: {enemy.hp}")
     print("-" * 30)
-    action = input("Pilih aksi (att/heal/skill): ")
+    
+    action = input("Pilih aksi (basic/skill1/skill2/heal) : ")
+    
+    
     if action == "att":
       hero.attack_target(enemy)
       time.sleep(1)
       if enemy.is_alive():
           enemy.attack_target(hero)
       time.sleep(1)
+      
+    elif action == "skill1":
+      success = hero.skill_1(enemy)
+      if success:
+        if enemy.is_alive():
+          enemy.attack_target(hero)
+        time.sleep(1)
+      else:
+        continue
+
+      
+    elif action == "skill2":
+      success = hero.skill_2(enemy)
+      if success:
+        if enemy.is_alive():
+          enemy.attack_target(hero)
+        time.sleep(1)
+      else:
+        continue
+      
     elif action == "heal":
       hero.hp += 10
       print(f"{hero.name} heal +10 HP!")
       if enemy.is_alive():
           enemy.attack_target(hero)
       time.sleep(1)
-    elif action == "skill":
-      if hero.sp > 0:
-          hero.use_skill(enemy)
-          if enemy.is_alive():
-              enemy.attack_target(hero)
-          time.sleep(1)
-      else:
-          print("SP tidak cukup! Pilih aksi lain.")
-          continue
+      
+      
       
       
     else:
